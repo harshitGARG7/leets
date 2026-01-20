ba696423442c3c085c38a0a6661023b58d6dcfe3
harshitGARG7 <88076253+harshitGARG7@users.noreply.github.com>
2026-01-20 23:01:39 +0530

    fixed

--- dailyProblems/minBitwiseArray.java
+++ dailyProblems/minBitwiseArray.java
@@ -4,14 +4,24 @@ class Solution {
         int[] ans = new int[n];
 
         for (int i = 0; i < n; i++) {
-            int x = nums.get(i);
-            // If even, impossible
-            if ((x & 1) == 0) {
+            int p = nums.get(i);
+
+            // Even numbers are impossible
+            if ((p & 1) == 0) {
                 ans[i] = -1;
-            } else {
-                // Minimum value satisfying x = a | (a+1)
-                ans[i] = x - 1;
+                continue;
             }
+
+            // Count trailing 1s in p
+            int t = 0;
+            int temp = p;
+            while ((temp & 1) == 1) {
+                t++;
+                temp >>= 1;
+            }
+
+            // Minimum a
+            ans[i] = p - (1 << (t - 1));
         }
         return ans;
     }
