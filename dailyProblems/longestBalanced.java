b778aa0d90f2688004dcd737853f69298f7bef59
harshitGARG7 <88076253+harshitGARG7@users.noreply.github.com>
2026-02-10 20:02:29 +0530

    3719

---
+++ dailyProblems/longestBalanced.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestBalancedSubarray(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+
+        for (int l = 0; l < n; l++) {
+            Set<Integer> seen = new HashSet<>();
+            int balance = 0;
+
+            for (int r = l; r < n; r++) {
+                if (!seen.contains(nums[r])) {
+                    seen.add(nums[r]);
+                    if (nums[r] % 2 == 0) balance++;
+                    else balance--;
+                }
+
+                if (balance == 0) {
+                    ans = Math.max(ans, r - l + 1);
+                }
+            }
+        }
+        return ans;
+    }
+}
